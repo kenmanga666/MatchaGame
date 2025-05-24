952e5262c0ea8207fde177099cd0a8af3318df4b
kenmanga_ <kenny.vallee@ynov.com>
2025-05-24 13:27:23 +0200

    début code client + serveur (communication fonctionnelle)

---
+++ client/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <thread>
+#include <string>
+
+#pragma comment(lib, "ws2_32.lib")
+
+void receiveLoop(SOCKET socket) {
+    char buffer[1024];
+    while (true) {
+        int bytesReceived = recv(socket, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived <= 0) {
+            std::cout << "[x] Déconnexion du serveur.\n";
+            break;
+        }
+        buffer[bytesReceived] = '\0';
+        std::cout << "[Serveur] " << buffer;
+    }
+}
+
+int main() {
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(5000);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Erreur de connexion au serveur.\n";
+        return 1;
+    }
+
+    std::cout << "[✓] Connecté au serveur.\n";
+
+    std::thread receiver(receiveLoop, sock);
+    receiver.detach();
+
+    std::string input;
+    while (true) {
+        std::getline(std::cin, input);
+        if (input == "/quit") break;
+        send(sock, input.c_str(), input.size(), 0);
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
---
+++ server/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <winsock2.h>
+#include <thread>
+#include <vector>
+#include <mutex>
+
+#pragma comment(lib, "ws2_32.lib") // pour linker ws2_32
+
+std::vector<SOCKET> queue;
+std::mutex queue_mutex;
+
+void handleMatch(SOCKET player1, SOCKET player2) {
+    std::cout << "[*] Match lancé entre deux joueurs." << std::endl;
+
+    const char* start_msg = "MATCH_START\n";
+    send(player1, start_msg, strlen(start_msg), 0);
+    send(player2, start_msg, strlen(start_msg), 0);
+
+    // TODO : boucle de jeu (à implémenter plus tard)
+
+    closesocket(player1);
+    closesocket(player2);
+}
+
+void handleClient(SOCKET clientSocket) {
+    std::cout << "[+] Nouveau client connecté." << std::endl;
+
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        queue.push_back(clientSocket);
+
+        // Si deux clients sont en attente, lancer une partie
+        if (queue.size() >= 2) {
+            SOCKET p1 = queue[0];
+            SOCKET p2 = queue[1];
+            queue.erase(queue.begin(), queue.begin() + 2);
+            std::thread(handleMatch, p1, p2).detach();
+        }
+        else {
+            const char* wait_msg = "En attente d’un autre joueur...\n";
+            send(clientSocket, wait_msg, strlen(wait_msg), 0);
+        }
+    }
+}
+
+int main() {
+    WSADATA wsaData;
+    SOCKET serverSocket, clientSocket;
+    sockaddr_in serverAddr, clientAddr;
+    int clientSize = sizeof(clientAddr);
+
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Erreur socket()\n";
+        return 1;
+    }
+
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(5000);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    bind(serverSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    listen(serverSocket, SOMAXCONN);
+
+    std::cout << "[*] Serveur MatchaGame lancé sur le port 5000...\n";
+
+    while (true) {
+        clientSocket = accept(serverSocket, (sockaddr*)&clientAddr, &clientSize);
+        if (clientSocket != INVALID_SOCKET) {
+            std::thread(handleClient, clientSocket).detach();
+        }
+    }
+
+    closesocket(serverSocket);
+    WSACleanup();
+    return 0;
+}
