4eae9831d2ee6a9576eebec29a1f30d63be50991
kenmanga_ <kenny.vallee@ynov.com>
2025-06-01 16:01:54 +0200

    enlever le dossier de PFCNet le temps de finir TicTacNet (pour les builds)

--- client/PFCNet/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../engine/GameClient.hpp"
-#include "PFCGame.hpp"
-
-int main() {
-    GameClient client;
-    client.setGame(std::make_unique<PFCGame>());
-    client.run();
-    return 0;
-}
